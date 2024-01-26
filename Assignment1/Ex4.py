f77a17e26c36f149fccebaa55283a4dd0135996d
Seyyed Morteza Aghajanzadeh <mortezaaghajanzadeh@gmail.com>
2024-01-26 07:46:28 +0100

    Update Ex4.py

--- Assignment1/Ex4.py
+++ Assignment1/Ex4.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
-
+# import statsmodels.formula.api as sm
+import statsmodels.api as sm
 #%%
 data = pd.read_excel("Assignment1Data_G1.xlsx", sheet_name="Predictability")
 data = data.dropna()
@@ -14,3 +15,12 @@ periods = [int(str(i) + "0" + str(j)) for i in years for j in range(1,10) if len
 periods.extend([int(str(i) + str(j)) for i in years for j in range(1,13) if len(str(j)) == 2 ])
 periods.sort()
 periods
+# %% DP estimation
+predection_period = 200105
+in_sample_period = [i for i in periods if i < predection_period]
+in_sample_data = data[data["Month"].isin(in_sample_period)]
+# sm.ols(formula="ExcessRet ~ dp", data=in_sample_data).fit().params
+X = sm.add_constant(in_sample_data["dp"])
+y = in_sample_data["ExcessRet"]
+beta = sm.OLS(y,X).fit().params.to_numpy()
+X.iloc[-1].to_numpy() @ beta
